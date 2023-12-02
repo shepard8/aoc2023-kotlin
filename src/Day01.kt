50b523467a8457cf1f80e151ae2583f520c48beb
Fabian Pijcke <fabian.pijcke@gmail.com>
2023-12-02 14:58:23 +0100

    Day 1 better approach.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,26 +1,7 @@
 fun main() {
 
-    val numbersMap = mapOf(
-        "0" to 0,
-        "1" to 1,
-        "2" to 2,
-        "3" to 3,
-        "4" to 4,
-        "5" to 5,
-        "6" to 6,
-        "7" to 7,
-        "8" to 8,
-        "9" to 9,
-        "one" to 1,
-        "two" to 2,
-        "three" to 3,
-        "four" to 4,
-        "five" to 5,
-        "six" to 6,
-        "seven" to 7,
-        "eight" to 8,
-        "nine" to 9,
-    )
+    val numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
     fun part1(input: List<String>): Int {
         return input.sumOf { line ->
             line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
@@ -28,19 +9,11 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.sumOf { line ->
-            var first: Int? = null
-            var last = 0
-            for (i in line.indices) {
-                for (key in numbersMap.keys) {
-                    if (line.substring(i).startsWith(key)) {
-                        first = first ?: numbersMap[key]
-                        last = numbersMap[key] ?: 0
-                    }
-                }
-            }
-            (first ?: 0) * 10 + last
-        }
+        return part1(
+            input
+                .map { numbers.fold(it) { s, number -> s.replace(number, number + number.last()) } }
+                .map { numbers.foldIndexed(it) { i, s, number -> s.replace(number, "${i + 1}") } }
+        )
     }
 
     val input = readInput("Day01")
