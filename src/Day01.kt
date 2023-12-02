6a6a25d36d080d506a236bd0b9aa9c6e64864ecc
Fabian Pijcke <fabian.pijcke@gmail.com>
2023-12-02 14:12:54 +0100

    Day 1 first attempt.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,47 @@
 fun main() {
+
+    val numbersMap = mapOf(
+        "0" to 0,
+        "1" to 1,
+        "2" to 2,
+        "3" to 3,
+        "4" to 4,
+        "5" to 5,
+        "6" to 6,
+        "7" to 7,
+        "8" to 8,
+        "9" to 9,
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            var first: Int? = null
+            var last = 0
+            for (i in line.indices) {
+                for (key in numbersMap.keys) {
+                    if (line.substring(i).startsWith(key)) {
+                        first = first ?: numbersMap[key]
+                        last = numbersMap[key] ?: 0
+                    }
+                }
+            }
+            (first ?: 0) * 10 + last
+        }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
