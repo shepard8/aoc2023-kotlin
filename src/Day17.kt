649d314835f6474ae3a261550bad452554e9c96c
Fabian Pijcke <fabian.pijcke@gmail.com>
2023-12-18 05:51:37 +0100

    Day 17 solution

--- src/Day17.kt
+++ src/Day17.kt
@@ -30,8 +30,25 @@ fun main() {
                 LavaGridPosition(x, y - 1, Direction.Up, if (direction == Direction.Up) countInDirection + 1 else if (direction == Direction.Down) 4 else 1),
             ).filter { it.countInDirection < 4 && grid.contains(it.x, it.y) }
         }
-    }
 
+        fun nextPositionsUltra(grid: LavaGrid): List<LavaGridPosition> {
+            if (countInDirection < 4) {
+                return listOf(
+                        if (direction == Direction.Right) LavaGridPosition(x + 1, y, direction, countInDirection + 1)
+                        else if (direction == Direction.Left) LavaGridPosition(x - 1, y, direction, countInDirection + 1)
+                        else if (direction == Direction.Down) LavaGridPosition(x, y + 1, direction, countInDirection + 1)
+                        else if (direction == Direction.Up) LavaGridPosition(x, y - 1, direction, countInDirection + 1)
+                        else error("")
+                ).filter { it.x != grid.width - 1 || it.y != grid.height - 1 || it.countInDirection > 3 }
+            }
+            return listOf(
+                LavaGridPosition(x + 1, y, Direction.Right, if (direction == Direction.Right) countInDirection + 1 else if (direction == Direction.Left) 11 else 1),
+                LavaGridPosition(x - 1, y, Direction.Left, if (direction == Direction.Left) countInDirection + 1 else if (direction == Direction.Right) 11 else 1),
+                LavaGridPosition(x, y + 1, Direction.Down, if (direction == Direction.Down) countInDirection + 1 else if (direction == Direction.Up) 11 else 1),
+                LavaGridPosition(x, y - 1, Direction.Up, if (direction == Direction.Up) countInDirection + 1 else if (direction == Direction.Down) 11 else 1),
+            ).filter { it.countInDirection < 11 && grid.contains(it.x, it.y) }.filter { it.x != grid.width - 1 || it.y != grid.height - 1 || it.countInDirection > 3 }
+        }
+    }
 
     fun part1(input: List<String>): Long {
         val grid = LavaGrid(input)
@@ -42,36 +59,60 @@ fun main() {
         nextPositionsToExplore.add(Pair(start, 0))
         while (nextPositionsToExplore.isNotEmpty()) {
             val (p, cost) = nextPositionsToExplore.poll()
-            println("${nextPositionsToExplore.size}, $cost")
 
             if ((bestCostTo[p] ?: Long.MAX_VALUE) < cost) {
                 continue
             }
             bestCostTo[p] = cost
-            p.nextPositions(grid).forEach {
-                nextPositionsToExplore.offer(Pair(it, grid.at(it.x, it.y) + cost))
+            p.nextPositions(grid).forEach { p2 ->
+                val p2cost = grid.at(p2.x, p2.y) + cost
+                val p3 = nextPositionsToExplore.find { it.first == p2 }
+                if (p3 != null && p3.second > p2cost) {
+                    nextPositionsToExplore.remove(p3)
+                    nextPositionsToExplore.offer(Pair(p2, p2cost))
+                }
+                else if (p3 != null) {
+
+                }
+                else {
+                    nextPositionsToExplore.offer(Pair(p2, p2cost))
+                }
             }
         }
-//        for (y in 0..<grid.height) {
-//            for (x in 0..<grid.width) {
-//                print(bestCostTo[Pair(x, y)]!!.cost)
-//                print(", ")
-//            }
-//            println()
-//        }
-//
-//        println()
-//        println()
-//        var p = bestCostTo[Pair(grid.width - 1, grid.height - 1)]
-//        while (p != null) {
-//            println(p)
-//            p = p.previous
-//        }
         return bestCostTo.entries.filter { it.key.x == grid.width - 1 && it.key.y == grid.height - 1 }.map { it.value }.min()
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        val grid = LavaGrid(input)
+        val start = LavaGridPosition(0, 0, Direction.Left, 5)
+        val bestCostTo = HashMap<LavaGridPosition, Long>()
+        bestCostTo[start] = 0
+        val nextPositionsToExplore = PriorityQueue<Pair<LavaGridPosition, Long>>(Comparator.comparing<Pair<LavaGridPosition, Long>?, Long?> { it.second })
+        nextPositionsToExplore.add(Pair(start, 0))
+        while (nextPositionsToExplore.isNotEmpty()) {
+            val (p, cost) = nextPositionsToExplore.poll()
+
+            if ((bestCostTo[p] ?: Long.MAX_VALUE) < cost) {
+                continue
+            }
+            bestCostTo[p] = cost
+            p.nextPositionsUltra(grid).forEach { p2 ->
+                val p2cost = grid.at(p2.x, p2.y) + cost
+                val p3 = nextPositionsToExplore.find { it.first == p2 }
+                if (p3 != null && p3.second > p2cost) {
+                    nextPositionsToExplore.remove(p3)
+                    nextPositionsToExplore.offer(Pair(p2, p2cost))
+                }
+                else if (p3 != null) {
+
+                }
+                else {
+                    nextPositionsToExplore.offer(Pair(p2, p2cost))
+                }
+            }
+        }
+        return bestCostTo.entries.filter { it.key.x == grid.width - 1 && it.key.y == grid.height - 1 }.map { it.value }.min()
+
     }
 
     val input = readInput("Day17")
