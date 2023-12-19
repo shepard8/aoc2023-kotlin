ea04a85d8df7080f35ce762d9a2ed949a056ecd3
Fabian Pijcke <fabian.pijcke@gmail.com>
2023-12-19 05:56:24 +0100

    Day 18 solution

--- src/Day18.kt
+++ src/Day18.kt
@@ -5,13 +5,23 @@ import javax.imageio.ImageIO
 
 fun main() {
 
-    data class DigPosition(val x: Int, val y: Int) {
+    data class DigPosition(val x: Long, val y: Long) {
         fun nextInDirection(direction: String): DigPosition {
             return when (direction) {
                 "U" -> DigPosition(x, y + 1)
                 "D" -> DigPosition(x, y - 1)
                 "L" -> DigPosition(x - 1, y)
                 "R" -> DigPosition(x + 1, y)
+                else -> error("")
+            }
+        }
+
+        fun nextInDirection(direction: String, length: Long): DigPosition {
+            return when (direction) {
+                "U" -> DigPosition(x, y + length)
+                "D" -> DigPosition(x, y - length)
+                "L" -> DigPosition(x - length, y)
+                "R" -> DigPosition(x + length, y)
                 else -> error("")
             }
         }
@@ -32,12 +42,12 @@ fun main() {
 
         }
 
-        val minX = dug.minOf { it.x }
-        val minY = dug.minOf { it.y }
-        val maxX = dug.maxOf { it.x }
-        val maxY = dug.maxOf { it.y }
-        val polygonXs = dug.map { it.x - minX }.toIntArray()
-        val polygonYs = dug.map { it.y - minY }.toIntArray()
+        val minX = dug.minOf { it.x }.toInt()
+        val minY = dug.minOf { it.y }.toInt()
+        val maxX = dug.maxOf { it.x }.toInt()
+        val maxY = dug.maxOf { it.y }.toInt()
+        val polygonXs = dug.map { (it.x - minX).toInt() }.toIntArray()
+        val polygonYs = dug.map { (it.y - minY).toInt() }.toIntArray()
         val image = BufferedImage(maxX - minX + 1, maxY - minY + 1, BufferedImage.TYPE_INT_RGB);
         val g = image.createGraphics();  // not sure on this line, but this seems more right
         g.color = Color.white
@@ -55,6 +65,30 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
+        var lastPos = DigPosition(0, 0)
+        val vertices = mutableListOf<DigPosition>(lastPos)
+        var circ = 0L
+
+        input.forEach { line ->
+            val (_, _, color) = line.split(" ")
+            val direction = when (color.drop(2).dropLast(1).last()) {
+                '0' -> "R"
+                '1' -> "D"
+                '2' -> "L"
+                '3' -> "U"
+                else -> error("")
+            }
+            val length = color.drop(2).dropLast(2).toInt(16)
+            lastPos = lastPos.nextInDirection(direction, length.toLong())
+            vertices.add(lastPos)
+            if (direction in listOf("L", "D")) circ += length.toLong()
+        }
+
+        val area2 = - (vertices.mapIndexed { index, p ->
+            p.x * vertices[(index + 1) % vertices.size].y - (vertices[(index + 1) % vertices.size]).x * p.y
+        }).sum() / 2 + circ + 1
+        println(area2)
+
         return 0
     }
 
