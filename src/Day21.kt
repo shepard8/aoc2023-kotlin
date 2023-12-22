2201f2cef22b445334618fc646b9785ce8169c46
Fabian Pijcke <fabian.pijcke@gmail.com>
2023-12-22 05:53:39 +0100

    Day 21 solution

--- src/Day21.kt
+++ src/Day21.kt
@@ -2,7 +2,6 @@ import day21.Grid
 import day21.DijkstraDistances
 import kotlin.math.abs
 
-
 fun main() {
 
     fun part1(input: List<String>): Long {
@@ -20,24 +19,14 @@ fun main() {
         if (tileX == 0 || tileY == 0) {
             return (131 * abs(tileX + tileY) - 65)
         }
-        return (131 * abs(tileX) - 65 + 131 * abs(tileY) - 65)
-    }
-
-    fun maxDistanceFor(tileX: Int, tileY: Int): Int {
-        if (tileX == 0 && tileY == 0) {
-            return 65 + 65
-        }
-        if (tileX == 0 || tileY == 0) {
-            return minDistanceFor(abs(tileX + tileY) + 1, 0) - 1 + 65
-        }
-        return minDistanceFor(abs(tileX) + 1, abs(tileY) + 1) - 1
+        error("")
     }
 
     val maxSteps = 26501365
 
-    fun count(grid: Grid, startX: Int, startY: Int, initDistance: Int, even: Int): Int {
+    fun count(grid: Grid, startX: Int, startY: Int, initDistance: Int): Long {
         val distances = DijkstraDistances(grid, grid.cellAt(startX, startY))
-        return distances.values.count { it + initDistance <= maxSteps && (it + initDistance) % 2 == even }
+        return distances.values.count { it + initDistance <= maxSteps && (it + initDistance) % 2 == 1 }.toLong()
     }
 
     fun part2(input: List<String>): Long {
@@ -45,60 +34,60 @@ fun main() {
 
         val perpendicularLast = 202300
         val minDistancePerpendicular = minDistanceFor(0, perpendicularLast)
-        println(minDistancePerpendicular)
-        println(maxDistanceFor(0, perpendicularLast))
+        println("minDistancePerpendicular $minDistancePerpendicular")
         println()
 
         var total = 0L
 
         // Top partially covered
-        val topCount = count(grid, 65, 130, minDistancePerpendicular, 1)
+        val topCount = count(grid, 65, 130, minDistancePerpendicular)
         println("topCount $topCount")
         total += topCount
 
         // Right partially covered
-        val rightCount = count(grid, 0, 65, minDistancePerpendicular, 1)
+        val rightCount = count(grid, 0, 65, minDistancePerpendicular)
         println("rightCount $rightCount")
         total += rightCount
 
         // Bottom partially covered
-        val bottomCount = count(grid, 65, 0, minDistancePerpendicular, 1)
+        val bottomCount = count(grid, 65, 0, minDistancePerpendicular)
         println("bottomCount $bottomCount")
         total += bottomCount
 
         // Left partially covered
-        val leftCount = count(grid, 130, 65, minDistancePerpendicular, 1)
+        val leftCount = count(grid, 130, 65, minDistancePerpendicular)
         println("leftCount $leftCount")
         total += leftCount
 
+        println()
+
         // Inside cells
-        val evenCount = count(grid, 0, 0, 0, 0)
-        println("evenCount $evenCount")
-        val oddCount = count(grid, 0, 0, 0, 1)
-        println("oddCount $oddCount")
-        total += evenCount // Center tile
-        (1..<perpendicularLast).forEach { d ->
-            if (d % 2 == 0)
-                total += evenCount * (4 * d - 4) + 4 * oddCount
-            else
-                total += oddCount * (4 * d - 4) + 4 * evenCount
+        val congruentStartCount = count(grid, 65, 65, 0)
+        println("evenCount $congruentStartCount")
+        val nonCongruentStartCount = count(grid, 65, 65, 1)
+        println("oddCount $nonCongruentStartCount")
+        total += congruentStartCount // Center tile
+        total += (1..<perpendicularLast).sumOf { d ->
+            (if (d % 2 == 0) congruentStartCount else nonCongruentStartCount) * 4 * d
         }
 
+        println()
+
         // TopRight border
-        total += perpendicularLast * count(grid, 0, 130, minDistancePerpendicular + 66, 1).also { println("topRightSmall $it") }
-        total += (perpendicularLast - 1) * count(grid, 0, 130, minDistancePerpendicular - 65, 0).also { println("topRightBig $it") }
+        total += perpendicularLast * count(grid, 0, 130, minDistancePerpendicular + 66).also { println("topRightSmall $it") }
+        total += (perpendicularLast - 1) * count(grid, 0, 130, minDistancePerpendicular - 65).also { println("topRightBig $it") }
 
         // BottomRight border
-        total += perpendicularLast * count(grid, 0, 0, minDistancePerpendicular + 66, 1).also { println("bottomRightSmall $it") }
-        total += (perpendicularLast - 1) * count(grid, 0, 0, minDistancePerpendicular - 65, 0).also { println("bottomRightBig $it") }
+        total += perpendicularLast * count(grid, 0, 0, minDistancePerpendicular + 66).also { println("bottomRightSmall $it") }
+        total += (perpendicularLast - 1) * count(grid, 0, 0, minDistancePerpendicular - 65).also { println("bottomRightBig $it") }
 
         // BottomLeft border
-        total += perpendicularLast * count(grid, 130, 0, minDistancePerpendicular + 66, 1).also { println("bottomLeftSmall $it") }
-        total += (perpendicularLast - 1) * count(grid, 130, 0, minDistancePerpendicular - 65, 0).also { println("bottomLeftBig $it") }
+        total += perpendicularLast * count(grid, 130, 0, minDistancePerpendicular + 66).also { println("bottomLeftSmall $it") }
+        total += (perpendicularLast - 1) * count(grid, 130, 0, minDistancePerpendicular - 65).also { println("bottomLeftBig $it") }
 
         // TopLeft border
-        total += perpendicularLast * count(grid, 130, 130, minDistancePerpendicular + 66, 1).also { println("topLeftSmall $it") }
-        total += (perpendicularLast - 1) * count(grid, 130, 130, minDistancePerpendicular - 65, 0).also { println("topLeftBig $it") }
+        total += perpendicularLast * count(grid, 130, 130, minDistancePerpendicular + 66).also { println("topLeftSmall $it") }
+        total += (perpendicularLast - 1) * count(grid, 130, 130, minDistancePerpendicular - 65).also { println("topLeftBig $it") }
 
         return total
     }
@@ -115,3 +104,9 @@ fun main() {
 // 54 662 951 064 921 incorrect
 // 54 662 926 384 565 incorrect
 // 54 662 897 657 965 incorrect
+// 54 662 931 037 465 incorrect
+// 54 662 906 356 614 incorrect
+// 54 662 881 676 014
+// 54 662 881 675 953
+// 54 662 939 736 139
+//618 261 433 219 147
